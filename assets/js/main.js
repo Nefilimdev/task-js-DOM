9acd92a5d84511d0318d149851c63937e6d42c70
Caio Alves <caiojuodzevicius@gmail.com>
2019-12-13 01:06:39 -0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+//let h1 = document.getElementById("cabecalho");
+//h1.style.display = 'none';
+
+    
+/* let gray =
+document.querySelector(".img");
+gray.style.filter = "grayscale(100%)";
+
+<span class="gray"><img id="lechuza" src="images/pic00.jpg" alt="" /></span> */
+
+
+/* let input =
+document.querySelectorAll(".input")’;
+for (let i = 0; i < input.length; i++) {
+ input[i].style.background = 'red'
+}
+
+input.style.background = "red"; */
